613fcb9be72e76910c013026c894b25892dd0e42
Juan <juangui9508@hotmail.com>
2024-01-02 00:50:30 -0500

    settings

--- rick_morty/src/redux/actions.js
+++ rick_morty/src/redux/actions.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
 export const GET_CHARACTERS = "GET_CHARACTERS";
+// Variables entorno
+
+const back = import.meta.env.VITE_APP_BACK;
 
 export const getCharacters = () => {
   return async function (dispatch) {
     try {
-      const apiData = await axios.get("http://localhost:3001/characters/");
+      const apiData = await axios.get(`${back}/characters`);
       const characters = apiData.data;
       dispatch({ type: GET_CHARACTERS, payload: characters });
     } catch (error) {
