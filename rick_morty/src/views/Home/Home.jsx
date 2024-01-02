7c55a1b34d2a7564b0e47e8e76f321ece3a5971a
Juan <juangui9508@hotmail.com>
2024-01-02 00:32:42 -0500

    styles

--- rick_morty/src/views/Home/Home.jsx
+++ rick_morty/src/views/Home/Home.jsx
@@ -3,8 +3,7 @@ import { useDispatch } from "react-redux"
 import { useEffect } from "react";
 import { getCharacters } from "../../redux/actions";
 import Cards from "../../components/Cards/Cards";
-
-
+import style from "../Home/Home.module.css";
 
 const Home = () =>{
     const dispatch = useDispatch();
@@ -15,10 +14,8 @@ const Home = () =>{
   
     },[dispatch]);
 
-
-
   return(
-     <div>
+     <div className={style.homeStyle}>
       <h1>Rick and Morty</h1>
       <Cards/>   
 
